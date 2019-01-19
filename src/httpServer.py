7e58138ab51af1feea2e6d2e963e78abcd7f6890
WYANG008 <weitao.yang@finbook.co>
2019-01-20 03:09:53 +0800

    update

--- src/httpServer.py
+++ src/httpServer.py
@@ -21,23 +21,7 @@ with open('model/bow_transformer.pickle', 'rb') as f:
 with open('model/tfidf_transformer.pickle', 'rb') as f:
     tfidf_transformer = pickle.load(f)
 
-# db = DynamoUtil(aws_keys['credentials']['accessKeyId'], aws_keys['credentials']['secretAccessKey'], aws_keys['region'])
-
-# def text_process(comments):
-#     nopunc = [char for char in comments if char not in string.punctuation ]
-#     nopunc = ''.join(nopunc)
-
-#     return [word for word in nopunc.split()
-#           if word.lower not in stopwords.words('english')
-#           and word.lower not in stopwords.words('german')]
-
 def evaluate(addr):
-    # relatedAddrs = etherscan.getRelatedAddresses(addr)
-    # dbAddrs = [addr['address'] for addr in db.scanIdentifiedAddress()]
-
-    # print('ehterscan related addresses: ',relatedAddrs)
-    # print('identified scam address in DB: ', dbAddrs)
-    # dummyScore = 0
     comments = getAddressComments(addr)
     X_test =pd.DataFrame([comments])
     test_bow = bow_transformer.transform(X_test['comments'])
@@ -51,10 +35,9 @@ def evaluate(addr):
         'score':score,
         'comments':comments
     }
-    return json.dumps(response, indent=4)
+    # return json.dumps(response, indent=4)
+    return score
 
-
- 
 @app.route("/")
 def query():
     score = None
@@ -71,12 +54,3 @@ def query():
  
 app.run(port=CST.SCORE_ENGINE_HTTP_SERVER_PORT)
 
-
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello():
-#     return "Hello World!"
-
-# app.run(port=3001)
